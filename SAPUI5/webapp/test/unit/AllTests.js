ba5a69e19a15e361ec2807e269826759b6d34fe6
Sergio Ortiz <sergiohoo@gmail.com>
2021-03-13 16:09:58 +0000

    Crea AllTests.js y unitTests.qunit.js

---
+++ SAPUI5/webapp/test/unit/AllTests.js
@@ -0,0 +1,6 @@
+sap.ui.define([
+    "namespace/SAPUI5/test/unit/model/InvoicesFormatter"
+],
+    function () {
+        "use strict";
+    })
---
+++ SAPUI5/webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/**
+ * global QUnit
+ */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+    "use strict";
+
+    sap.ui.require([
+        "namespace/SAPUI5/test/unit/AllTests"
+    ], function () {
+        QUnit.start();
+    })
+});
