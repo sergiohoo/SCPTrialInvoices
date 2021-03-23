6933a64af5db8395f192eef0100145b0f4b19af2
Sergio Ortiz <sergiohoo@gmail.com>
2021-03-23 13:14:19 +0000

    Crea vista Details

---
+++ SAPUI5/webapp/controller/Details.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller'
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        return Controller.extend("namespace.SAPUI5.controller.Details", {
+            onInit: function () {
+
+            }
+        })
+    });    
