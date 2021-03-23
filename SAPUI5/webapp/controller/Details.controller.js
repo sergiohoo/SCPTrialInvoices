4407abe3a0c43a4b9196ba74fa20a7b4e7fc74b5
Sergio Ortiz <sergiohoo@gmail.com>
2021-03-23 16:24:54 +0000

    Agrega vista de atributos en details

--- SAPUI5/webapp/controller/Details.controller.js
+++ SAPUI5/webapp/controller/Details.controller.js
@@ -7,7 +7,16 @@ sap.ui.define([
     function (Controller) {
         return Controller.extend("namespace.SAPUI5.controller.Details", {
             onInit: function () {
-
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("Details").attachPatternMatched(
+                    this._onObjectMatch, this);
+            },
+            _onObjectMatch: function (oEvent) {
+                this.getView().bindElement({
+                    path: "/" + window.decodeURIComponent(
+                        oEvent.getParameter("arguments").invoicePath),
+                    model: "northwind"
+                })
             }
         })
-    });    
+    });
--- SAPUI5/webapp/controller/InvoicesList.controller.js
+++ SAPUI5/webapp/controller/InvoicesList.controller.js
@@ -35,6 +35,14 @@ sap.ui.define([
                 const oBinding = oList.getBinding("items");
 
                 oBinding.filter(aFilter);
+            },
+            navigateToDetails: function (oEvent) {
+                const oItem = oEvent.getSource();
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("Details", {
+                    invoicePath: window.encodeURIComponent(
+                        oItem.getBindingContext("northwind").getPath().substr(1))
+                });
             }
         })
     });
