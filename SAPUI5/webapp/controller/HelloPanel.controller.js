c49e95f8b7382651a0b5d2d9bf4471751d0dfa20
Sergio Ortiz <sergiohoo@gmail.com>
2021-04-20 16:43:39 +0000

    Laboratorio 12 terminado

--- SAPUI5/webapp/controller/HelloPanel.controller.js
+++ SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,23 +1,36 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.base.Log} Log
      * */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         return Controller.extend("namespace.SAPUI5.controller.HelloPanel", {
             onInit: function () {
 
             },
-            onShowHello : function () {
+
+            onBeforeRendering: function () {
+                window.message = "Log message - onBeforeRendering";
+                Log.info(window.message);
+                Log.error(window.message);
+            },
+
+            onAfterRendering: function () {
+                //debugger;
+            },
+
+            onShowHello: function () {
                 var oBundle = this.getView().getModel("i18n").getResourceBundle();
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
             },
-            onOpenDialog : function () {
+            onOpenDialog: function () {
                 this.getOwnerComponent().openHelloDialog();
             }
         })
